42a00d1a4703e2d077a94ae4ad1c9a7952e6dd65
Mayuri Pachpor <vishalpatil080502@gmail.com>
2022-12-03 15:28:44 +0530

    User Dashboard Added

---
+++ src/components/UserDashboard/UserDashboard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import OriginButton from '../../Helpers/originButton/OriginButton'
+import MyProfile from '../MyProfile/MyProfile'
+import "./UserDashBoard.css"
+const UserDasBoard = () => {
+
+    
+  return (
+    <>
+    <div className='wrapContent'>
+      <div id='borderContainer'>
+      <MyProfile/>
+      <div id='imageTagContainer'>
+        <h1>Matches</h1>
+      <img id='profile-pic' className="profile-pic"
+      src="https://cdn.discordapp.com/attachments/963207924656795680/1027005573499199609/3982230923_Gigachad.png"
+      alt='profile-image' />
+
+      </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default UserDasBoard
